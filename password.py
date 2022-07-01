2b69536bdeecb9866c7925da152912db93f365d3
unknown <derdahyounes.pro@gmail.com>
2022-07-01 10:11:18 +0200

    second commit

--- password.py
+++ password.py
@@ -1,5 +1,7 @@
 import random
+##choose the length of your password
 s = int(input("choose the length of your password"))
+##string of numbers, uppercase, lowercase and characters
 p = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYXZ123456789@*#%&^£$?"
 jn = "".join(random.sample(p, s))
 print(jn)
